7cfd9929994e3edcc1fed032346f11a4cf01654b
AyrtomCav <franciscoayrton123@gmail.com>
2026-05-12 11:35:00 -0300

    feat: integrar login SUAP e rotas

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from apps.authentication.views import HealthCheckView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('health/', HealthCheckView.as_view(), name='health-check'),
+    path('api/auth/', include('apps.authentication.urls')),
 ]
